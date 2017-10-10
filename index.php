80c3234ce1d84c40a1abc335a87e5d196e913386
cmink-alone <cmink.alone@gmail.com>
2017-10-10 14:11:06 +0800

    add index and pust data into database

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
 <?php
-    require_once("koneksi.php");
+require_once("koneksi.php");
+
+$db = new SQLiteDatabase('test.sqlite');
+//membuka file database sqlite sekaligus membuat file baru jika file tidak ditemukan
+$count = $db->get_table_count();
+//mendapatkan jumlah tabel pada database
+if($count < 1){
+	//j	ika jumlah tabel kurang dari 1
+	//b	uat tabel baru dengan nama users dan membuat array baru untuk tiap kolom database
+	$db->create_table("users",array('id' => 'INTEGER(10)',
+	'nama' => 'VARCHAR(50)'
+	));
+	
+}
+
+//Menambahan Data ke tabel users
+$db->add_data('users',array('id'=>'1','nama'=>'Muhammad Sayuti'));
+
+//Menampilkan Data dalam array
+print_r($db->get_data('users'));
+
+//Mengubah nama user dengan id 1
+if($db->update_data('users',array('nama'=>'Sukirman'),array('id'=>'1'))){
+	
+	echo "<br />";
+	
+	print_r($db->get_data('users'));
+	
+}
+
+//menghapus semua data pada tabel users
+if($db->delete_data('users')){
+	
+	print_r('<br />data berhasil dihapus');
+	
+}
+
 ?>
